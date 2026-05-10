3194cb66fecebc25db081af895c66140941fef5f
Daniel Castillo <dcastillogi@outlook.com>
2026-05-09 22:37:41 -0500

    chore: initial project scaffold

---
+++ test/aws-cdk-cicd-kit.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkCicdKit from '../lib/aws-cdk-cicd-kit-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-cicd-kit-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkCicdKit.AwsCdkCicdKitStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
